5ad46a10044b91ff4b67bb65f517941858fcd10f
Mason Benzinger <mason.benzinger@gmail.com>
2022-09-12 20:16:54 -0500

    merged wth suspence

--- src/App.js
+++ src/App.js
@@ -1,45 +1,53 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
+import { createResource as fetchData } from './helper'
 
 
-const App = () => {
-  let [search, setSearch] = useState('')
-  let [message, setMessage] = useState('Search for Music!')
-  let [data, setData] = useState([])
 
-  const API_URL = 'https://itunes.apple.com/search?term='
 
-  useEffect(() => {
-    if (search) {
-      const fetchData = async () => {
-        document.title = `${search} music`
-        const response = await fetch(API_URL + search)
-        const resData = await response.json()
-        if (resData.results.length > 0) {
-          return setData(resData.results)
-        } else {
-          return setMessage('Not Found.')
+function App() {
+    let [search, setSearch] = useState('')
+    let [message, setMessage] = useState('Search for Music!')
+    let [data, setData] = useState(null)
+
+    const API_URL = 'https://itunes.apple.com/search?term='
+
+    useEffect(() => {
+        if (searchTerm) {
+            setData(fetchData(searchTerm))
         }
-      }
-      fetchData()
+    }, [searchTerm])
+
+
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        setSearch(term)
     }
-  }, [search])
 
-  const handleSearch = (e, term) => {
-    e.preventDefault()
-    setSearch(term)
-  }
-console.log('this is my search', search)
-console.log('this is my data', data)
-
-  return (
-    <div>
-      <SearchBar handleSearch={handleSearch} />
-      {message}
-      <Gallery data={data} />
-    </div>
-  )
+    return (
+        <div>
+            {message}
+            <Router>
+                <Routes>
+                    <Route path="/" element={
+                        //<Fragment>
+                        <div>
+                            <SearchBar handleSearch={handleSearch} />
+                            <Gallery data={data} />
+                        </div>
+                        //</Fragment>
+                    } />
+                    <Route path="/album/:id" element={<AlbumView />} />
+                    <Route path="/artist/:id" element={<ArtistView />} />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
-export default App
+export default App;
+
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,11 @@
+import { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
-function Gallery(props) {
+const Gallery = (props) => {
+    const data = props.data.result.read()
 
-    const display = props.data.map((item, index) => {
+    const display = data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
